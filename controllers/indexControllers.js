67341977a13595bd48e457d8050dbf9f93499903
Chris Padilla <cdpadilla42@gmail.com>
2020-04-13 15:25:46 -0500

    Write signup POST controller

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,3 +1,6 @@
+const { body, validationResults, sanitizeBody } = require('express-validator');
+const User = require('../models/user');
+
 exports.indexGet = (req, res) => {
   res.send('NOT IMPLEMENTED: indexGet');
 };
@@ -6,9 +9,46 @@ exports.signUpGet = (req, res) => {
   res.render('sign-up');
 };
 
-exports.signUpPost = (req, res) => {
-  res.send('NOT IMPLEMENTED: signUpPost');
-};
+exports.signUpPost = [
+  // validate
+  body('first_name').trim().isLength({ min: 1 }),
+  body('last_name').trim().isLength({ min: 1 }),
+  body('username').trim().isLength({ min: 1 }),
+  body('password').trim().isLength({ min: 1 }),
+
+  // sanitize
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('last_name').escape(),
+  sanitizeBody('username').escape(),
+  sanitizeBody('password').escape(),
+  sanitizeBody('membership').trim().escape(),
+
+  // process request
+  (req, res, next) => {
+    // TODO: VALIDATE PASSWORD
+    // Create new user
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      // TODO: ENCRYPT PASSWORD
+      password: req.body.password,
+      member_status: req.body.member_status,
+    });
+    // handle errors
+    const errors = validationResults(req);
+    if (!errors.isEmpty()) {
+      // return to form w/ errors
+      res.render('sign-up', { errors: errors.array() });
+    } else {
+      // view success
+      user.save((err, newUser) => {
+        if (err) return next(err);
+        res.redirect('/');
+      });
+    }
+  },
+];
 
 exports.loginGet = (req, res) => {
   res.send('NOT IMPLEMENTED: loginGet');
